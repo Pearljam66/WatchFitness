a7608e7cfec1c586f23817f099ca9ce9c4895384
Sarah Clark <sarahireneclark@gmail.com>
2023-11-10 14:36:07 -0600

    Initial commit of watch project

---
+++ WatchFitness/WatchFitness Watch App/WatchFitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchFitnessApp.swift
+//  WatchFitness Watch App
+//
+//  Created by Sarah Clark on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct WatchFitness_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
