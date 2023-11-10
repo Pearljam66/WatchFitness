7dca2010ba88c4c68e9b9be865ea0e7346952fa1
Sarah Clark <sarahireneclark@gmail.com>
2023-11-10 15:10:01 -0600

    Added basic activity UI

--- WatchFitness/WatchFitness Watch App/ContentView.swift
+++ WatchFitness/WatchFitness Watch App/ContentView.swift
@@ -8,12 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var counter = 0
+    @State private var timerString = "00:00:00"
+    @State private var bpm = 120
+    @State private var calories = 110
+    @State private var activity = "Running"
+
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack (alignment: .leading) {
+            Text(timerString)
+                .font(.title2)
+                .foregroundColor(Color.yellow)
+                .padding(.bottom)
+                .onReceive(timer) {time in
+                counter += 1
+
+                let hours = counter / 3600
+                let minutes = (counter % 3600) / 60
+                let seconds = counter % 3600 % 60
+
+                timerString = String(format: "%02d", hours) + ":" + String(format: "%02d", minutes) + ":" + String(format:"%02d", seconds)
+            }
+
+            Text("00:15:42")
+            Text("120 BPM")
+            Text("110 kcal")
+            Text("Running")
         }
         .padding()
     }
